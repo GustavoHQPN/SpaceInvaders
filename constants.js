a26447762d5699bf531dc0046302f12c189e8d3d
Gustavo Henrique <121318659+GustavoHQPN@users.noreply.github.com>
2024-12-09 11:51:20 -0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,13 @@
+export const PATH_SPACESHIP_IMAGE = "src/assets/images/Captura de tela 2024-12-02 200910.png";
+export const PATH_ENGINE_IMAGE = "src/assets/images/Captura de tela 2024-12-02 200910.png";
+export const PATH_ENGINE_SPRITES = "src/assets/images/Captura de tela 2024-12-02 200910.png";
+export const PATH_INVADER_IMAGE = "src/assets/images/invader.png";
+
+export const NUMBER_STARS = 100;
+export const INITIAL_FRAMES = 8;
+
+export const GameState = {
+    START: "start",
+    PLAYING: "playing",
+    GAME_OVER: "gameOver",
+};
